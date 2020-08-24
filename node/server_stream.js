30245c4ecbc3c50a39b6a7ed0e80354fdfc5e74b
Crystal <fhy_crystal@163.com>
2020-08-24 11:00:29 +0800

    新增流操作、gz压缩

---
+++ node/server_stream.js
@@ -0,0 +1,29 @@
+let http = require('http');
+let fs = require('fs');
+let zlib = require('zlib');
+
+let server = http.createServer((req, res) => {
+    // readFile 先把所有数据全读到内存，然后回调
+    // 1. 及其占用内存， 2.资源利用及其不充分（内存和磁盘运用不均）
+    // fs.readFile(`www${req.url}`, (err, data) => {
+    //     if (err) {
+    //         res.writeHead(404);
+    //         res.write('Not Found');
+    //     } else {
+    //         res.write(data);
+    //     }
+
+    //     res.end();
+    // })
+    res.setHeader('content-encoding', 'gzip');
+    let rs = fs.createReadStream(`www${req.url}`);
+    let gzip = zlib.createGzip();
+    rs.on('error', err => {
+        res.writeHead(404);
+        res.write('Not Found');
+        res.end();
+    })
+    rs.pipe(gzip).pipe(res);
+    // rs.pipe(res)
+})
+server.listen(8181);
