a7b2a4d1c320bd9242a8fb8e72111c2de7667e18
Crystal <fhy_crystal@163.com>
2020-09-14 17:12:08 +0800

    原生node后台新增跨域处理

--- node/formData/server.js
+++ node/formData/server.js
@@ -1,42 +1,47 @@
-// const http = require('http');
-// const queryString = require('querystring');
-// const urlLib = require('url');
+const http = require('http');
+const queryString = require('querystring');
+const urlLib = require('url');
 
-// http.createServer((req, res) => {
-//     let {
-//         pathname: url, 
-//         query: getData
-//     } = urlLib.parse(req.url, true)
-
-//     let arr = [];
-//     req.on('data', data => {
-//         arr.push(data);
-//     })
-//     req.on('end', () => {
-//         let postData = queryString.parse(Buffer.concat(arr).toString());
-//         console.log(url, getData, postData);
-//         res.end();
-//     })
+http.createServer((req, res) => {
+    let {
+        pathname: url, 
+        query: getData
+    } = urlLib.parse(req.url, true)
+    // req.headers['origin'] == 'null' || /^https?:\/\/(\w+\.)+aaa\.com/.test(req.headers['origin'])
+    // 文件调试或本地调试
+    if (req.headers['origin'] == 'null' || req.headers['origin'].startsWith('http://localhost')) {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+    }
     
-// }).listen(8066);
+    let arr = [];
+    req.on('data', data => {
+        arr.push(data);
+    })
+    req.on('end', () => {
+        let postData = queryString.parse(Buffer.concat(arr).toString());
+        console.log(url, getData, postData);
+        res.end();
+    })
+    
+}).listen(8066);
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const multer = require('multer')
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const multer = require('multer')
 
-let app = express();
-app.listen(8066);
+// let app = express();
+// app.listen(8066);
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-let multerObj = multer({
-    dest: './upload/'
-});
-app.use(multerObj.any())
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }))
+// let multerObj = multer({
+//     dest: './upload/'
+// });
+// app.use(multerObj.any())
 
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    console.log(req.files);
-    res.send('ok')
-})
+// app.post('/login', (req, res) => {
+//     console.log(req.body);
+//     console.log(req.files);
+//     res.send('ok')
+// })
